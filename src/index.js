15e5696db5daba1fa0cfde2262debbefe671ecbd
Gregtstu <boltnev.grisha@yandex.ru>
2022-08-14 09:58:31 +0300

    добавил стили и слайдер

--- src/index.js
+++ src/index.js
@@ -2,3 +2,30 @@ import './index.html';
 import './cart.html';
 import './card.html';
 import './index.scss';
+
+import Swiper, {Thumbs, Scrollbar, Pagination} from 'swiper';
+import 'swiper/css';
+import 'swiper/css/scrollbar';
+
+const thumbSwiper = new Swiper('.card__slider-thumb', {
+    spaceBetween: 44,
+    slidesPerView: 3,
+    scrollbar: {
+        el: '.swiper-scrollbar',
+        draggable:true,
+    },
+    modules: [Scrollbar]
+});
+new Swiper('.card__image', {
+    spaceBetween: 10,
+    slidesPerView: 1,
+    thumbs: {
+        swiper: thumbSwiper,
+        slideThumbActiveClass: '.card__thumb-btn_active',
+    },
+    modules: [Thumbs]
+});
+new Swiper('.recomended__carousel',{
+    spaceBetween: 30,
+    slidesPerView: 5,
+});
